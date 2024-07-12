fd53ec099821756ff33d12ed4d160d7d3978d759
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-07-12 21:38:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources-001"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
 
 resource "azurerm_log_analytics_workspace" "example" {
   name                = "example-log-analytics"
